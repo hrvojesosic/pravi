8dcf545d71005c1d68ad3eb08d80bd4c31ee788e
hrvojesosic <hsosic00@fesb.hr>
2026-03-24 19:16:12 +0100

    novaklasa

---
+++ NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pravi
+{
+    internal class Class1
+    {
+    }
+}
