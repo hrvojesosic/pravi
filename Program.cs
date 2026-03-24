0244661fd3db30d2f69561d36477f4412116129c
hrvojesosic <hsosic00@fesb.hr>
2026-03-24 19:06:52 +0100

    poruka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace pravi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Novo");
+        }
+    }
+}
